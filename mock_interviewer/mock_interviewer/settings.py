69d6a05d7648ff1f8e79a16d5a4d51153d42b7a9
sungbinlee <sunglee@addu.edu.ph>
2023-08-02 13:20:17 +0900

    [feat] 환경변수 분리

--- mock_interviewer/mock_interviewer/settings.py
+++ mock_interviewer/mock_interviewer/settings.py
@@ -12,19 +12,25 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import environ
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
+# Envirion setting
+env = environ.Env()
+environ.Env.read_env(
+    env_file=os.path.join(BASE_DIR, '.env')
+)
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-_#iz8wk(fjc898n*9t=-tzr*$h@*==tv!@3$8o6(vmsuviff4('
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
