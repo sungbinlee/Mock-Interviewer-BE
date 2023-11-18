b7b9e80bb1e23d63802dc8936fa4cb1aab36f5aa
Sungbin Lee <52542229+sungbinlee@users.noreply.github.com>
2023-11-18 21:42:15 +0900

    [test] 유저 기능 테스트 케이스 작성

--- mock_interviewer/chatbot/tests.py
+++ mock_interviewer/chatbot/tests.py
@@ -1,3 +1,52 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+from .models import User
 
-# Create your tests here.
+
+class AccountTests(APITestCase):
+    def test_create_account(self):
+        url = reverse('user-register')
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+            'password2': 'testpassword',
+            'email': 'test@test.com',
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+        self.assertEqual(User.objects.count(), 1)
+        self.assertEqual(User.objects.get().username, 'testuser')
+
+    def test_password_mismatch(self):
+        url = reverse('user-register')
+        data = {
+            'username': 'testuser',
+            'password': 'testpassword',
+            'password2': 'differentpassword',
+            'email': 'test@example.com'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_missing_fields(self):
+        url = reverse('user-register')
+        data = {
+            'username': 'testuser',
+            'email': 'test@example.com'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
+
+    def test_existing_username(self):
+        User.objects.create_user(username='existinguser', password='existingpassword')
+        
+        url = reverse('user-register')
+        data = {
+            'username': 'existinguser',
+            'password': 'testpassword',
+            'password2': 'testpassword',
+            'email': 'test@example.com'
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_409_CONFLICT)
